f09603ceb05bd30fd6a8db1f4ade2c90ab7c6acd
AndersonJS <ajs.ti@hotmail.com>
2019-03-07 14:36:10 -0400

    2.5 classe -Criando uma classe.

---
+++ OO/pessoa.py
@@ -0,0 +1,10 @@
+class Pessoa:
+    pass
+
+# Para importar uma classe e semelhante a uma função:
+from OO.pessoa import Pessoa
+p = Pessoa()
+type(p)
+# <class 'OO.pessoa.Pessoa'>
+
+print(__name__)
